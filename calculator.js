490846366f901dce1121d99c1ed43c349f0c7353
markutley <mark.utley.713@gmail.com>
2018-03-16 08:44:48 -0400

    buttons store variables

--- calculator.js
+++ calculator.js
@@ -1,11 +1,13 @@
+
+
 function add(a, b){
-  sum = a + b;
-  return sum;
+  answer = a + b;
+  return answer;
 };
 
 function subtract(a, b){
-  sum = a - b;
-  return sum;
+  answer = a - b;
+  return answer;
 };
 
 function multiply(a, b){
@@ -21,10 +23,10 @@ function divide(a, b){
 function operate(a, b, operator){
   if(operator === "+"){
     add(a, b);
-    return sum;
+    return answer;
   } else if(operator === "-"){
     subtract(a, b);
-    return sum;
+    return answer;
   } else if(operator === "*"){
     multiply(a, b);
     return answer;
@@ -33,3 +35,46 @@ function operate(a, b, operator){
     return answer;
   };
 };
+
+const display = document.getElementById("display");
+
+for(i = 0; i < 10; i++){
+  var button = document.getElementById(i);
+  button.addEventListener("click", function(event){
+    if(display.textContent === "+" || display.textContent === "-" ||
+      display.textContent === "*" || display.textContent === "/"){
+        operator = display.textContent;
+      }
+    display.textContent = event.target.id;
+  });
+};
+
+const plus = document.getElementById("+");
+plus.addEventListener("click", function(event){
+  a = parseInt(display.textContent);
+  display.textContent = event.target.id;
+});
+
+const minus = document.getElementById("-");
+minus.addEventListener("click", function(event){
+  a = parseInt(display.textContent);
+  display.textContent = event.target.id;
+});
+
+const times = document.getElementById("*");
+times.addEventListener("click", function(event){
+  a = parseInt(display.textContent);
+  display.textContent = event.target.id;
+});
+
+const dividedBy = document.getElementById("/");
+dividedBy.addEventListener("click", function(event){
+  a = parseInt(display.textContent);
+  display.textContent = event.target.id;
+});
+
+const equals = document.getElementById("equals");
+equals.addEventListener("click", function(){
+  var b = parseInt(display.textContent);
+  display.textContent = operate(a, b, operator);
+});
