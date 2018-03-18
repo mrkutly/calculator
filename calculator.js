9d417287b5006df1acadf6a30565dced16b5578c
markutley <mark.utley.713@gmail.com>
2018-03-18 14:00:01 -0400

    final js file

--- calculator.js
+++ calculator.js
@@ -1,4 +1,4 @@
-
+//creating my mathematic functions//
 function add(a, b){
   answer = a + b;
   return answer;
@@ -19,7 +19,14 @@ function divide(a, b){
   return answer;
 };
 
-function operate(a, b, operator){
+//A function to round my final answers to a certain decimal place:
+
+function round(value, decimals) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+//A function that takes input and figures out which mathematic function to run//
+function operate(operator, a, b){
   if(operator === "+"){
     add(a, b);
     return answer;
@@ -35,80 +42,75 @@ function operate(a, b, operator){
   };
 };
 
+//*** Building the calculator *** //
+
 const display = document.getElementById("display");
 
-for(i = 0; i < 10; i++){
+//loop to add event listeners to the number buttons
+for(i = 0; i < 10; i++)
+{
   var button = document.getElementById(i);
-  button.addEventListener("click", function(event){
-    if(display.textContent === "+" || display.textContent === "-" ||
-      display.textContent === "*" || display.textContent === "/"){
-        operator = display.textContent;
-        display.textContent = event.target.id;
-      } else {
+  button.addEventListener("click", function(event)
+  {
     display.textContent += event.target.id;
-  }
   });
 };
 
-const plus = document.getElementById("+");
-plus.addEventListener("click", function(event){
-  if(typeof(a) != "undefined"){
-    b = parseInt(display.textContent);
-    display.textContent = operate(a, b, operator);
-    a = parseInt(display.textContent);
-    display.textContent = event.target.id;
-  } else {
-    a = parseInt(display.textContent);
-    display.textContent = event.target.id;
-  };
-});
+//getting operator buttons from DOM//
+const plusButton = document.getElementById("+");
+const minusButton = document.getElementById("-");
+const timesButton = document.getElementById("*");
+const divisionButton = document.getElementById("/");
 
-const minus = document.getElementById("-");
-minus.addEventListener("click", function(event){
-  if(typeof(a) != "undefined"){
-    b = parseInt(display.textContent);
-    display.textContent = operate(a, b, operator);
-    a = parseInt(display.textContent);
-    display.textContent = event.target.id;
-  } else {
-  a = parseInt(display.textContent);
-  display.textContent = event.target.id;
-  }
-});
-
-const times = document.getElementById("*");
-times.addEventListener("click", function(event){
-  if(typeof(a) != "undefined"){
-    b = parseInt(display.textContent);
-    display.textContent = operate(a, b, operator);
-    a = parseInt(display.textContent);
-    display.textContent = event.target.id;
-  } else {
-  a = parseInt(display.textContent);
-  display.textContent = event.target.id;
-  }
-});
-
-const dividedBy = document.getElementById("/");
-dividedBy.addEventListener("click", function(event){
-  if(typeof(a) != "undefined"){
-    b = parseInt(display.textContent);
-    display.textContent = operate(a, b, operator);
-    a = parseInt(display.textContent);
-    display.textContent = event.target.id;
-  } else {
-  a = parseInt(display.textContent);
-  display.textContent = event.target.id;
-  }
-});
-
-const equals = document.getElementById("equals");
-equals.addEventListener("click", function(){
-  var b = parseInt(display.textContent);
-  display.textContent = operate(a, b, operator);
-});
-
+//getting clear and return buttons from DOM//
 const clear = document.getElementById("clear");
-clear.addEventListener("click", function(){
+const equals = document.getElementById("equals");
+
+//adding event listeners to update display with operators//
+plusButton.addEventListener("click", function (event){
+  display.textContent += " " + event.target.id + " ";
+});
+
+minusButton.addEventListener("click", function (event){
+  var str = display.textContent
+  if (str.charAt(str.length - 1) == " " || str == "")
+  {
+    display.textContent += event.target.id
+  } else
+  {
+    display.textContent += " " + event.target.id + " ";
+  }
+});
+
+timesButton.addEventListener("click", function (event){
+  display.textContent += " " + event.target.id + " ";
+});
+
+divisionButton.addEventListener("click", function (event){
+  display.textContent += " " + event.target.id + " ";
+});
+
+clear.addEventListener("click", function (event){
   display.textContent = "";
-})
+});
+
+//comparing the contiguous array indeces to keep track of currentAnswer.
+//Just like kadane's algorithm!!! learning is fun!!!
+
+equals.addEventListener("click", function()
+{
+  var equationString = display.textContent;
+  var equationArray = equationString.split(" ");
+  var currentAnswer = Number(equationArray[0]);
+  for (i = 0; i < equationArray.length; i++)
+  {
+    if(i > 0 && i % 2 == 0)
+    {
+      newAnswer = operate(
+        equationArray[i - 1], currentAnswer, Number(equationArray[i])
+      );
+      currentAnswer = newAnswer;
+      display.textContent = round(currentAnswer, 5);
+    }
+  }
+});
